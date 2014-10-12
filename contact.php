aa11f2e649ccd5757c61dc55bf8fbc73b472ff55
agc3sz <agc3sz@virginia.edu>
2014-10-11 22:55:53 -0400

    New Template Added

--- contact.php
+++ contact.php
@@ -9,11 +9,10 @@
 <body>
 <?php
 $name = $_GET['name'];
-$date = $_GET['date'];
 $email = $_GET['email'];
 $message = $_GET['message'];
 
-mail("agc3sz@virginia.edu","Message from $email on $date",$message);
+mail("agc3sz@virginia.edu","Message from $email",$message);
 echo "Thank You $name for your message!";
 ?>
 
